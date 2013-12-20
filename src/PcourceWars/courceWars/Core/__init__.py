2a134abd95a252119128c2a629eeed0ae2d4aef1
sanslash332 <sanslash332@yahoo.es>
2013-12-20 04:33:15 -0300

    Añadido primer esqueleto del proyecto.

---
+++ src/PcourceWars/courceWars/Core/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+""" módulo inicializador del paquete Core. El paquete tiene las clases, módulos y funciones principales para el funcionamiento del núcleo del juego."""
---
+++ src/PcourceWars/courceWars/courceWars.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+""" CourceWars: fighter en desarrollo para el proyecto recistencia arcade.
+Módulo principal, se encarga de llevar el control y flujo del juego, además de conectar el núcleo con la visualización"""
+
+
+
+
